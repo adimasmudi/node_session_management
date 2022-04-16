f287a82bcf130b5561d5f5a65f0f693b81a94bdf
adimasmudi <adimasmudi@gmail.com>
2022-04-16 12:33:26 +0700

    finish this simple project

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
-const sessions = require("express-session");
+const sessions = require('express-session');
 
 const app = express();
 const PORT = 4000;
 
 const oneDay = 1000 * 60 * 60 * 24;
 
+//session middleware
 app.use(sessions({
-    secret : "thisismysecrctekeyfhrgfgrfrty84fwir767",
-    saveUninitialized : true,
-    cookie : {maxAge : oneDay},
-    resave : false
+    secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+    saveUninitialized:true,
+    cookie: { maxAge: oneDay },
+    resave: false
 }));
 
 // parsing the incoming data
@@ -26,7 +27,7 @@ app.use(cookieParser());
 
 // username and password
 const myusername = 'user1';
-const password = 'mypassword';
+const mypassword = 'mypassword';
 
 // a variable to save a session
 var session;
@@ -41,10 +42,9 @@ app.get('/',(req,res)=>{
 });
 
 app.post('/user',(req,res)=>{
-    if(req.body.username == myusername && req.body.password == password){
+    if(req.body.username == myusername && req.body.password == mypassword){
         session=req.session;
         session.userid=req.body.username;
-        console.log(req.body)
         res.send(`Hey There, welcome <a href=\'/logout'>click to logout</a>`);
     }else{
         res.send('Invalid username or password');
@@ -53,7 +53,7 @@ app.post('/user',(req,res)=>{
 
 app.get('/logout',(req, res)=>{
     req.session.destroy();
-    req.redirect('/');
+    res.redirect('/');
 });
 
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
